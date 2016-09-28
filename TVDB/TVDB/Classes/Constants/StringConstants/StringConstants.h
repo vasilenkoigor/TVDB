b8543f868387b5580122883030861b215f173068
Igor Vasilenko <ivasilenko@yotateam.com>
2016-09-28 11:48:06 +0300

    Added new storyboard and StringConstants

---
+++ TVDB/TVDB/Classes/Constants/StringConstants/StringConstants.h
@@ -0,0 +1,9 @@
+//
+// Created by Igor Vasilenko on 28/09/2016.
+// Copyright (c) 2016 Igor Vasilenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const kTwitterConsumerAPIKey;
+extern NSString *const kTwitterConsumerAPISecret;
---
+++ TVDB/TVDB/Classes/Constants/StringConstants/StringConstants.m
@@ -0,0 +1,9 @@
+//
+// Created by Igor Vasilenko on 28/09/2016.
+// Copyright (c) 2016 Igor Vasilenko. All rights reserved.
+//
+
+#import "StringConstants.h"
+
+NSString *const kTwitterConsumerAPIKey = @"xZ1VwG2r6FybVqRDLzFeC5oGB";
+NSString *const kTwitterConsumerAPISecret = @"5gAprfdFiHW0O8iVSiZuSLjSU0V2Q6eKcGmttFO0wCC8OLNc6e";
